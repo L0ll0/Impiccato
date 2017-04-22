9de54fcee47d08f0d2df0531eafa669038e4754d
WinEnzo <vincenzosusso@gmail.com>
2017-04-22 17:51:18 +0200

    "Aggiunta Formattazione"

--- ImpiccatoGame/src/impiccatogame/ImpiccatoGame.java
+++ ImpiccatoGame/src/impiccatogame/ImpiccatoGame.java
@@ -1,21 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @author WinLollo
  */
+
 package impiccatogame;
 
-/**
- *
- * @author Arduino
- */
-public class ImpiccatoGame {
+public class ImpiccatoGame 
+{
+    public static void main(String[] args) 
+    {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    } 
 }
